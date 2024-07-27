9b71c16bbc4e5ef402be59920f0a88e6dd8f96cc
kartik <codebull707@gmail.com>
2024-07-28 00:02:15 +0530

    feat: relate panelists to organizations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_27_092138) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_27_183048) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,8 +31,11 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_27_092138) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "organization_id", null: false
     t.index ["email"], name: "index_panelists_on_email", unique: true
+    t.index ["organization_id"], name: "index_panelists_on_organization_id"
     t.index ["reset_password_token"], name: "index_panelists_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "panelists", "organizations"
 end
