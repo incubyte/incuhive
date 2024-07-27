770e2274c741cc69082a5fc8621fa5d80b770dc8
kartik <codebull707@gmail.com>
2024-07-27 16:04:44 +0530

    chore: schema file rearrange

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,15 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_27_092138) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "organizations", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "invite_code", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["invite_code"], name: "index_organizations_on_invite_code", unique: true
+    t.index ["name"], name: "index_organizations_on_name", unique: true
+  end
+
   create_table "panelists", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -24,15 +33,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_27_092138) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_panelists_on_email", unique: true
     t.index ["reset_password_token"], name: "index_panelists_on_reset_password_token", unique: true
-  end
-
-  create_table "organizations", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "invite_code", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["invite_code"], name: "index_organizations_on_invite_code", unique: true
-    t.index ["name"], name: "index_organizations_on_name", unique: true
   end
 
 end
