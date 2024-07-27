76867c6453895196cef77e6745a338dc2f032973
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-07-27 15:59:06 +0530

    feat: add auth to home page

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -11,7 +11,40 @@ RSpec.describe 'home/index.html.erb' do
   it { expect(rendered).to have_content('Streamlining the hiring process for your team') }
 
   it { expect(rendered).to have_content('Why Choose Incuhive?') }
-  it { expect(rendered).to have_button('Get Started') }
+
+  context 'when panelist is signed in' do
+    before do
+      panelist_double = instance_double(Panelist, email: 'test@example.com')
+      allow(view).to receive_messages(
+        panelist_signed_in?: true,
+        current_panelist: panelist_double
+      )
+      render
+    end
+
+    it 'greets the signed-in panelist' do
+      expect(rendered).to have_content('Welcome test@example.com')
+    end
+
+    it 'shows the sign out button' do
+      expect(rendered).to have_button('Sign out')
+    end
+  end
+
+  context 'when panelist is not signed in' do
+    before do
+      allow(view).to receive(:panelist_signed_in?).and_return(false)
+      render
+    end
+
+    it 'shows the get started button' do
+      expect(rendered).to have_button('Get Started')
+    end
+
+    it 'does not show the sign out button' do
+      expect(rendered).to have_no_button('Sign out')
+    end
+  end
 
   it 'displays the feature description for feature 1' do
     expect(rendered).to have_content('Easy to use')
