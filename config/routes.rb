a23e7fd3a1b05c7a3006cbd4a46f6f4473feab1b
kartik <codebull707@gmail.com>
2024-07-27 14:37:34 +0530

    feat: organizations show route

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root 'home#index'
-  resources :organizations, only: [:new, :create]
+  resources :organizations, only: [:new, :create, :show]
 end
