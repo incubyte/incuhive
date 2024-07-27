df63eecff8bf3368d71747ff5e1383e27f1d337a
kartik <codebull707@gmail.com>
2024-07-27 23:39:28 +0530

    refactor: simplify controller logic

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class OrganizationsController < ApplicationController
-  before_action :organization, only: %i[show update regenerate_invite_code]
+  before_action :set_organization, only: %i[show update regenerate_invite_code]
 
   def show; end
 
@@ -11,48 +11,35 @@ class OrganizationsController < ApplicationController
 
   def create
     @organization = Organization.new(organization_params)
-    @organization.invite_code = generate_invite_code
     if @organization.save
-      flash[:notice] = 'Organization created successfully.'
-      redirect_to new_organization_path
+      redirect_to new_organization_path, notice: 'Organization created successfully.'
     else
-      flash[:alert] = 'Error creating organization.'
-      render new_organization_path
+      flash[:alert] = @organization.errors.full_messages.to_sentence
+      redirect_to new_organization_path
     end
-  rescue ActiveRecord::RecordNotUnique
-    flash[:alert] = 'Organization name must be unique.'
-    redirect_to new_organization_path
   end
 
   def update
     if @organization.update(organization_params)
-      flash[:notice] = 'Organization updated successfully.'
+      redirect_to organization_path(@organization), notice: 'Organization updated successfully.'
     else
-      flash[:alert] = 'Error updating organization.'
+      flash[:alert] = @organization.errors.full_messages.to_sentence
+      redirect_to organization_path(@organization)
     end
-    redirect_to organization_path(@organization)
-  rescue ActiveRecord::RecordNotUnique
-    flash[:alert] = 'Organization name must be unique.'
-    redirect_to organization_path(@organization)
   end
 
   def regenerate_invite_code
-    @organization.update(invite_code: generate_invite_code)
-    flash[:notice] = 'Invite code regenerated successfully.'
-    redirect_to organization_path(@organization)
+    @organization.regenerate_invite_code
+    redirect_to organization_path(@organization), notice: 'Invite code regenerated successfully.'
   end
 
   private
 
-  def organization
-    @organization ||= Organization.find(params[:id])
+  def set_organization
+    @organization = Organization.find(params[:id])
   end
 
   def organization_params
     params.require(:organization).permit(:name)
-  end
-
-  def generate_invite_code
-    SecureRandom.hex(10)
   end
 end
