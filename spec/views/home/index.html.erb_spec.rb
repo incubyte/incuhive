c839c5acea11d8ad3f379040b910033464091dbc
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-07-27 13:38:22 +0530

    feat: replace css with tailwind

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -10,14 +10,8 @@ RSpec.describe 'home/index.html.erb' do
   it { expect(rendered).to have_content('Welcome to Incuhive') }
   it { expect(rendered).to have_content('Streamlining the hiring process for your team') }
 
-  it 'displays the call-to-action button' do
-    expect(rendered).to have_button('Get Started')
-  end
-
-  it 'displays the feature section' do
-    expect(rendered).to have_content('Why Choose Incuhive?')
-    expect(rendered).to have_css('.feature', count: 2)
-  end
+  it { expect(rendered).to have_content('Why Choose Incuhive?') }
+  it { expect(rendered).to have_button('Get Started') }
 
   it 'displays the feature description for feature 1' do
     expect(rendered).to have_content('Easy to use')
@@ -26,11 +20,6 @@ RSpec.describe 'home/index.html.erb' do
 
   it 'displays the feature description for feature 2' do
     expect(rendered).to have_content('Excel Interop')
-    expect(rendered).to have_content('Incuhive can both import and export to excel')
-  end
-
-  it 'ensures feature cards have equal height using inline styles' do
-    rendered_height_styles = Nokogiri::HTML(rendered).css('.feature').pluck('style')
-    expect(rendered_height_styles.uniq.size).to eq(1)
+    expect(rendered).to have_content('Incuhive can both import and export to Excel')
   end
 end
