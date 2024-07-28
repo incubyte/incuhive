d37c9032a472f91b01cc218abe3cf119c75df4eb
kartik <codebull707@gmail.com>
2024-07-28 12:41:58 +0530

    refactor: we dont need admin

--- db/migrate/20240727190039_add_admin_to_organizations.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddAdminToOrganizations < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :organizations, :admin, null: true, foreign_key: { to_table: :panelists }
-  end
-end
