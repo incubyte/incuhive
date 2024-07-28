15147f7748b74951f6786f972ac6d7a60a5c0a1f
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-07-28 13:59:31 +0530

    feat: add panelist seed and factory

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+organization = FactoryBot.create(:organization)
+
+5.times do
+  FactoryBot.create(:panelist, organization: organization)
+end
--- spec/factories/panelists.rb
+++ spec/factories/panelists.rb
@@ -2,8 +2,16 @@
 
 FactoryBot.define do
   factory :panelist do
+    name { Faker::Name.name }
     email { Faker::Internet.email }
     password { 'password123' }
     password_confirmation { 'password123' }
+    available_time_slots do
+      [Faker::Time.forward(days: 5, period: :morning).strftime('%A %H-%M'),
+       Faker::Time.forward(days: 5, period: :afternoon).strftime('%A %H-%M')]
+    end
+    organization
+    expertise { Faker::Job.field }
+    bio { Faker::Lorem.paragraph }
   end
 end
