4314f018731d2a4bb0faaf79d8192f26be738ba4
kartik <codebull707@gmail.com>
2024-07-28 16:39:11 +0530

    feat: gen candidate views
    Co-authored-by: Abhishek Keshri <iam2kabhishek@gmail.com>

---
+++ app/views/candidates/_candidate.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! candidate, :id, :name, :email, :current_role, :applied_role, :location, :total_experience, :relevant_experience, :skills, :notice_period, :resume_link, :linkedin_link, :github_link, :repo_link, :company_name, :feedback, :offer_given, :created_at, :updated_at
+json.url candidate_url(candidate, format: :json)
---
+++ spec/factories/candidates.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :candidate do
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
+    current_role { 'MyString' }
+    applied_role { 'MyString' }
+    location { 'MyString' }
+    total_experience { 1 }
+    relevant_experience { 1 }
+    skills { 'MyString' }
+    notice_period { 1 }
+    resume_link { 'MyString' }
+    linkedin_link { 'MyString' }
+    github_link { 'MyString' }
+    repo_link { 'MyString' }
+    company_name { 'MyString' }
+    feedback { 'MyText' }
+    offer_given { false }
+
+    organization
+  end
+end
