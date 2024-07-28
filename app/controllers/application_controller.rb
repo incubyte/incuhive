7965cd718a7271a92fe448b5281b11f8a964c89e
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-07-28 14:42:55 +0530

    feat: update after sign in path
    Co-authored-by: kartik <codebull707@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,11 @@
 
 class ApplicationController < ActionController::Base
   # Override Devise's after_sign_in_path_for method
-  def after_sign_in_path_for(_resource)
-    create_or_join_organizations_path
+  def after_sign_in_path_for(resource)
+    if resource.organization.nil?
+      create_or_join_organizations_path
+    else
+      organization_path(resource.organization)
+    end
   end
 end
